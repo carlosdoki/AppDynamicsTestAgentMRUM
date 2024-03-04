1b3bb07a5b62a5a8f9e32c5aa8a7faa291ec7b30
Carlos Doki <carlosdoki@gmail.com>
2024-03-04 17:35:41 -0300

    Agent AppDynamics

--- index.js
+++ index.js
@@ -2,8 +2,15 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
+
+import { Instrumentation } from '@appdynamics/react-native-agent';
+
+Instrumentation.start({
+  appKey: 'GR-AAB-CWF',
+  collectorURL: 'https://gru-col.eum-appdynamics.com',
+});
 
 AppRegistry.registerComponent(appName, () => App);
